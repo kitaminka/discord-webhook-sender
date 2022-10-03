255d9350b83d2768c6f4e61472ad242938105848
kitaminka <kit4minka@outlook.com>
2022-10-03 20:04:31 +0300

    Create ErrorMessage

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,40 @@
+<template>
+  <transition>
+    <p class="error-message" v-if="show">
+      <slot/>
+    </p>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'ErrorMessage',
+  props: {
+    show: {
+      default: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.error-message {
+  color: #fc6661;
+  font-size: 14px;
+  margin: 5px 0;
+  max-height: 17px;
+}
+
+.v-enter-active {
+  transition: opacity 0.4s ease, max-height 0.2s ease, margin 0.2s ease;
+}
+.v-leave-active {
+  transition: opacity 0.2s ease, max-height 0.4s ease, margin 0.4s ease;
+}
+.v-enter-from,
+.v-leave-to {
+  max-height: 0;
+  opacity: 0;
+  margin: 0;
+}
+</style>
--- src/components/WebhookSettings.vue
+++ src/components/WebhookSettings.vue
@@ -3,9 +3,7 @@
     <h3 class="webhook-settings__header">Webhook settings</h3>
     <p>Webhook URL*</p>
     <app-input class="webhook-settings__input" placeholder="https://discord.com/api/webhooks/xxx/xxx..." v-model="webhookUrl"/>
-    <transition>
-      <p class="webhook-settings__invalid-webhook-message" v-if="!validWebhook && webhookUrl !== ''">Invalid Webhook URL</p>
-    </transition>
+    <error-message :show="!validWebhook && webhookUrl !== ''">Invalid Webhook URL</error-message>
     <p>Username</p>
     <app-input class="webhook-settings__input" placeholder="Captain Hook" v-model="username"/>
     <app-button :disabled="!validWebhook" @click="sendMessage" class="webhook-settings__send-button">{{sendButtonText}}</app-button>
@@ -17,10 +15,12 @@ import { mapState, mapActions } from 'vuex';
 
 import AppButton from '@/components/AppButton';
 import AppInput from '@/components/AppInput';
+import ErrorMessage from '@/components/ErrorMessage';
 
 export default {
   name: 'WebhookSettings',
   components: {
+    ErrorMessage,
     AppButton,
     AppInput
   },
@@ -70,27 +70,9 @@ export default {
   box-sizing: border-box;
   width: 100%;
 }
-.webhook-settings__invalid-webhook-message {
-  color: #fc6661;
-  font-size: 14px;
-  margin: 5px 0;
-  max-height: 17px;
-}
 .webhook-settings__send-button {
   margin-top: 10px;
+  margin-bottom: 5px;
   box-sizing: border-box;
-}
-
-.v-enter-active {
-  transition: opacity 0.4s ease, max-height 0.2s ease, margin 0.2s ease;
-}
-.v-leave-active {
-  transition: opacity 0.2s ease, max-height 0.4s ease, margin 0.4s ease;
-}
-.v-enter-from,
-.v-leave-to {
-  max-height: 0;
-  opacity: 0;
-  margin: 0;
 }
 </style>
