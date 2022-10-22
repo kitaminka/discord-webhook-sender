3f9c028e89b9a8e74f1d9ee9dfb867635052ef60
kitaminka <kit4minka@outlook.com>
2022-10-22 21:36:20 +0300

    Update button variants

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -28,5 +28,16 @@ export default {
   border-radius: 5px;
   font-family: 'Roboto', sans-serif;
   cursor: pointer;
+  background-color: var(--button-background-color);
+}
+.button:hover {
+  background-color: var(--button-background-color-hover);
+}
+.button:active {
+  background-color: var(--button-background-color-active);
+}
+.button:disabled {
+  cursor: not-allowed;
+  background-color: #706f6f;
 }
 </style>
