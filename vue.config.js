2191e565277388a6eebd12963e84a9ba33fa4046
kitaminka <kit4minka@gmail.com>
2023-09-02 16:17:18 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/discord-webhook-sender'
-        : '/'
+    publicPath: '/'
 });
